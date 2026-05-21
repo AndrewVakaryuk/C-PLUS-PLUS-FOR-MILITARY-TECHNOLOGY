ba69f31f3477fa46668d945d18dfd6f940ae35c0
Andrii Vakariuk <andreyvakaryuk@gmail.com>
2026-05-21 17:15:43 +0000

    Homework 6 - added script to run commands
    Added script to run format, build, lint or all at once in one command. Applied format on the codebase.

--- homework_06/include/ballistics.hpp
+++ homework_06/include/ballistics.hpp
@@ -3,34 +3,34 @@
 #include <string_view>
 
 struct BallisticsInput {
-    double drone_x = 0.0;
-    double drone_y = 0.0;
-    double drone_z = 0.0;
-    double target_x = 0.0;
-    double target_y = 0.0;
-    double attack_speed = 0.0;
-    double acceleration_path = 0.0;
-    std::string_view ammo_name;
+  double drone_x = 0.0;
+  double drone_y = 0.0;
+  double drone_z = 0.0;
+  double target_x = 0.0;
+  double target_y = 0.0;
+  double attack_speed = 0.0;
+  double acceleration_path = 0.0;
+  std::string_view ammo_name;
 };
 
 struct DropSolution {
-    double fire_x = 0.0;
-    double fire_y = 0.0;
-    bool maneuver_needed = false;
-    double maneuver_x = 0.0;
-    double maneuver_y = 0.0;
+  double fire_x = 0.0;
+  double fire_y = 0.0;
+  bool maneuver_needed = false;
+  double maneuver_x = 0.0;
+  double maneuver_y = 0.0;
 };
 
 enum class BallisticsError {
-    UnknownAmmo,
-    InvalidModel,
-    NonPositiveFlightTime,
+  UnknownAmmo,
+  InvalidModel,
+  NonPositiveFlightTime,
 };
 
 struct BallisticsResult {
-    bool ok = false;
-    BallisticsError error = BallisticsError::UnknownAmmo;
-    DropSolution solution{};
+  bool ok = false;
+  BallisticsError error = BallisticsError::UnknownAmmo;
+  DropSolution solution{};
 };
 
 bool lookup_ammo_params(std::string_view ammo_name, double& mass, double& drag, double& lift);
